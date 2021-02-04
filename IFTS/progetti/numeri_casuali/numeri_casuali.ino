864b6e2ab322c4adbaad08a1570d47c605f95bfe
rechlo <lorenzorech98@gmail.com>
2021-02-04 11:23:49 +0100

    Update numeri_casuali.ino

--- IFTS/progetti/numeri_casuali/numeri_casuali.ino
+++ IFTS/progetti/numeri_casuali/numeri_casuali.ino
@@ -1,7 +1,7 @@
 void setup() {
-  randomSeed(analogRead(0))//creare codice di mescolamento casuale
+  randomSeed(analogRead(0));//creare codice di mescolamento casuale
 }
 
 void loop() {
-  random(60, 100)//si può mettere un solo numero come num.massimo o due il cui numero casuale sarà nell'intervallo fra i due
+  random(60, 100);//si può mettere un solo numero come num.massimo o due il cui numero casuale sarà nell'intervallo fra i due
 }
