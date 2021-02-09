aad1b706ed2fc2bb900abe1677a603081fdf1734
rechlo <lorenzorech98@gmail.com>
2021-02-09 12:37:03 +0100

    Update LCDdisplay-serial.ino

--- IFTS/progetti/LCDdisplay-serial/LCDdisplay-serial.ino
+++ IFTS/progetti/LCDdisplay-serial/LCDdisplay-serial.ino
@@ -1,12 +1,29 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 void setup() {
+  lcd.begin(16, 2);
+  // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.print("inserisci una stringa di max 16 caratteri");
+  Serial.println("inserisci una stringa di massimo 16 caratteri");
 }
+
 void loop() {
+  // put your main code here, to run repeatedly:
   if (Serial.available()) {
-    Serial.readString();
-    char stringa [20];
-    stringa =  Serial.readString();
-    Serial.print(stringa);
-    Serial.print("ok");
+    String stringa = Serial.readString();
+    Serial.println(stringa);
+    lcd.setCursor(0, 0);
+    lcd.print("                ");
+    //lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(stringa);
   }
+
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
