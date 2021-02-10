a1569a088cda2b096d7aa101ac5bdbd02253bf1f
rechlo <lorenzorech98@gmail.com>
2021-02-10 11:39:51 +0100

    Update SR04___ultrasuoni.ino

--- IFTS/progetti/SR04___ultrasuoni/SR04___ultrasuoni.ino
+++ IFTS/progetti/SR04___ultrasuoni/SR04___ultrasuoni.ino
@@ -1,12 +1,12 @@
 #include <LiquidCrystal.h>;
-LiquidCrystal lcd(1, 2, 4, 5,6,7);
+LiquidCrystal lcd(1, 2, 4, 5, 6, 7);
 const int trigPin = 9;
 const int echoPin = 10;
 long duration;
 int distanceCm ;
 
 void setup() {
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
@@ -18,10 +18,10 @@ void loop() {
   delay(10);
   digitalWrite(trigPin, LOW);
 
-  duration = pulseIn(echoPin,HIGH);
-             distanceCm = duration*0.034/2; 
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration * 0.034 / 2;
   lcd.setCursor(0, 0);
-lcd.print("Distance: ");
+  lcd.print("Distance: ");
   lcd.print(distanceCm);
   lcd.print(" cm");
   delay (10);
